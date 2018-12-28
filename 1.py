279f0a754b275043d331f86d0262852778dc71bc
Dylan Pham <32346117+pham-d@users.noreply.github.com>
2018-12-28 14:15:07 -0800

    updated 1.py and 2.py and added 5.py

--- 1.py
+++ 1.py
@@ -1,4 +1,4 @@
-# Multiples of 3 and 5
+# 1. Multiples of 3 and 5
 
 # If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
 
@@ -12,4 +12,4 @@ for i in range(0, 1000):
 
 print(sum)
 
-# 233168
+# answer = 233168
---
+++ 5.py
@@ -0,0 +1,20 @@
+# 5. Smallest multiple
+
+# 2520 is the smallest number that can be divided by each of the numbers from 1 to 10 without any remainder.
+
+# What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
+
+divisor = 20
+initialValue = 2520
+
+while True:
+    if initialValue % divisor == 0:
+        divisor -= 1
+        if divisor == 1:
+            print(initialValue)
+            break
+    else:
+        initialValue += 20
+        divisor = 20
+
+# answer = 232792560
