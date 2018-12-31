ae399cee9239ca7b1e71c4e2049ca5e7cedfbc05
Dylan Pham <32346117+pham-d@users.noreply.github.com>
2018-12-30 16:59:02 -0800

    Create 16.py

---
+++ 16.py
@@ -0,0 +1,14 @@
+# 16. Power digit sum
+
+# 215 = 32768 and the sum of its digits is 3 + 2 + 7 + 6 + 8 = 26.
+
+# What is the sum of the digits of the number 21000?
+
+num = 2**1000
+sum = 0
+for each in str(num):
+    sum += int(each)
+
+print(sum)
+
+# answer = 1336
