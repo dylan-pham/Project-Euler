de2f36686263ea88a6570b481538262ef0b63dc2
Dylan Pham <32346117+dylan-pham@users.noreply.github.com>
2020-05-23 22:44:03 -0700

    Add files via upload

---
+++ 67.py
@@ -0,0 +1,31 @@
+import time
+
+t0 = time.time()
+# ------------------------------
+
+# same as problem 18 except with a bigger triangle
+
+triangle = []
+largest_sum = [] # largest sum at a given point from bottom
+
+with open("67.txt") as file:
+    for line in file:
+        row = [int(num) for num in line.split(" ")];
+        triangle.append(row);
+        largest_sum.append([0 for i in range(len(row))])
+
+largest_sum[len(triangle)-1] = triangle[len(triangle)-1]
+
+for row in range(len(largest_sum)-2, -1, -1):
+    for col in range(len(largest_sum[row])):
+        largest_sum_left = largest_sum[row+1][col]
+        largest_sum_right = largest_sum[row+1][col+1]
+
+        largest_sum[row][col] = triangle[row][col] + (largest_sum_left if largest_sum_left > largest_sum_right else largest_sum_right)
+
+
+print(largest_sum[0][0])
+
+# ------------------------------
+t1 = time.time()
+print(f"program took {(t1-t0)*1000} milliseconds")
